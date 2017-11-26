eb23cff74d6346ddc9271c9b6342e69b80ff3a33
Dmitriy Zakharchuk <dmitry.zakh1@gmail.com>
2017-11-26 21:35:47 +0300

    Started new template for manage services

--- src/app/admin/admin-module.ts
+++ src/app/admin/admin-module.ts
@@ -4,7 +4,10 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { ManageServicesComponent } from './manage-services/manage-services.component';
 import { ManageUsersComponent} from './manage-users/manage-users.component';
-import {AdminHeaderComponent} from './admin-header/admin-header.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
+import { CategoryListComponent } from './manage-services/category-list/category-list.component';
+import { CategoryItemComponent } from './manage-services/category-item/category-item.component';
+import { CategoryBreadcrumbComponent } from './manage-services/category-breadcrumb/category-breadcrumb.component';
 
 @NgModule({
   imports: [
@@ -15,7 +18,10 @@ import {AdminHeaderComponent} from './admin-header/admin-header.component';
     AdminHomeComponent,
     ManageServicesComponent,
     ManageUsersComponent,
-    AdminHeaderComponent
+    AdminHeaderComponent,
+    CategoryBreadcrumbComponent,
+    CategoryListComponent,
+    CategoryItemComponent
   ]
 })
 export class AdminModule { }
---
+++ src/app/admin/manage-services/category-breadcrumb/category-breadcrumb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-breadcrumb',
+  templateUrl: './category-breadcrumb.component.html',
+  styleUrls: ['./category-breadcrumb.component.css']
+})
+export class CategoryBreadcrumbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
